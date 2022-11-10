95bbeca57532dc89375fe73694f1af060eeba661
Bogrintsev Vladimir <vbogrintsev@ozon.ru>
2022-11-10 15:43:23 +0400

    fixes

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"io"
 	"log"
+	"time"
 
 	pb "b2broker/pkg/b2brokerpb"
 
@@ -12,54 +13,53 @@ import (
 
 func main() {
 	ctx := context.Background()
-	// dial server
 	conn, err := grpc.Dial(":13111", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("can not connect with server %v", err)
 	}
 
-	// create client
 	client := pb.NewMessageServiceClient(conn)
 
-	// set connection stream for users
 	go clientByIDConnect(ctx, "111", client)
 	go clientByIDConnect(ctx, "222", client)
 	go clientByIDConnect(ctx, "333", client)
+
+	time.Sleep(time.Second * 5)
 
 	inCreateGroupChat := &pb.CreateGroupChatRequest{
 		ChatName: "chat10",
 		Username: "111",
 	}
 	outCreateGroupChat, err := client.CreateGroupChat(ctx, inCreateGroupChat)
-	log.Printf("CreateGroupChat: in %+v, out %+v", inCreateGroupChat, outCreateGroupChat)
+	log.Printf("CreateGroupChat: in %v, out %v, err %v", inCreateGroupChat, outCreateGroupChat, err)
 
 	inJoinGroupChat := &pb.JoinGroupChatRequest{
 		ChatName: "chat10",
 		Username: "222",
 	}
 	outJoinGroupChat, err := client.JoinGroupChat(ctx, inJoinGroupChat)
-	log.Printf("CreateGroupChat: in %+v, out %+v", inJoinGroupChat, outJoinGroupChat)
+	log.Printf("CreateGroupChat: in %v, out %v, err %v", inJoinGroupChat, outJoinGroupChat, err)
 
 	inJoinGroupChat2 := &pb.JoinGroupChatRequest{
 		ChatName: "chat10",
 		Username: "222",
 	}
 	outJoinGroupChat2, err := client.JoinGroupChat(ctx, inJoinGroupChat2)
-	log.Printf("CreateGroupChat: in %+v, out %+v", inJoinGroupChat2, outJoinGroupChat2)
+	log.Printf("CreateGroupChat: in %v, out %v, err %v", inJoinGroupChat2, outJoinGroupChat2, err)
 
 	inJoinGroupChat3 := &pb.JoinGroupChatRequest{
 		ChatName: "chat10",
 		Username: "333",
 	}
 	outJoinGroupChat3, err := client.JoinGroupChat(ctx, inJoinGroupChat3)
-	log.Printf("CreateGroupChat: in %+v, out %+v", inJoinGroupChat3, outJoinGroupChat3)
+	log.Printf("CreateGroupChat: in %v, out %v, err %v", inJoinGroupChat3, outJoinGroupChat3, err)
 
 	inLeftGroupChat := &pb.LeftGroupChatRequest{
 		ChatName: "chat10",
 		Username: "333",
 	}
 	outLeftGroupChat, err := client.LeftGroupChat(ctx, inLeftGroupChat)
-	log.Printf("LeftGroupChat: in %+v, out %+v", inLeftGroupChat, outLeftGroupChat)
+	log.Printf("LeftGroupChat: in %v, out %v, err %v", inLeftGroupChat, outLeftGroupChat, err)
 
 	inSendMessage := &pb.SendMessageRequest{
 		Data: &pb.Message{
@@ -69,27 +69,27 @@ func main() {
 		},
 	}
 	outSendMessage, err := client.SendMessage(ctx, inSendMessage)
-	log.Printf("SendMessage: in %+v, out %+v", inSendMessage, outSendMessage)
+	log.Printf("SendMessage: in %v, out %v, err %v", inSendMessage, outSendMessage, err)
 
 	inSendMessage2 := &pb.SendMessageRequest{
 		Data: &pb.Message{
 			SenderId:   "111",
-			ReceiverId: "chan10",
-			Payload:    "hello from 111 to 222",
+			ReceiverId: "chat10",
+			Payload:    "hello from 111 to chan10",
 		},
 	}
 	outSendMessage2, err := client.SendMessage(ctx, inSendMessage2)
-	log.Printf("SendMessage: in %+v, out %+v", inSendMessage2, outSendMessage2)
+	log.Printf("SendMessage: in %v, out %v, err %v", inSendMessage2, outSendMessage2, err)
 
 	inListChannels := &pb.ListChannelsRequest{}
 	outListChannels, err := client.ListChannels(ctx, inListChannels)
-	log.Printf("SendMessage: in %+v, out %+v", inListChannels, outListChannels)
+	log.Printf("SendMessage: in %v, out %v, err %v", inListChannels, outListChannels, err)
 
 	log.Printf("finished")
 }
 
 func clientByIDConnect(ctx context.Context, clientID string, client pb.MessageServiceClient) {
-	in := &pb.ConnectRequest{Username: "111"}
+	in := &pb.ConnectRequest{Username: clientID}
 	stream, err := client.Connect(ctx, in)
 	if err != nil {
 		log.Fatalf("open stream error %v", err)
@@ -101,7 +101,7 @@ func clientByIDConnect(ctx context.Context, clientID string, client pb.MessageSe
 		for {
 			resp, err := stream.Recv()
 			if err == io.EOF {
-				done <- true //means stream is finished
+				done <- true
 				return
 			}
 			if err != nil {
@@ -111,5 +111,5 @@ func clientByIDConnect(ctx context.Context, clientID string, client pb.MessageSe
 		}
 	}()
 
-	<-done //we will wait until all response is received
+	<-done
 }
